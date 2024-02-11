d976822bb930d2f5469092d07193fdbd19a90029
Fernando <101474411+FvCastr0@users.noreply.github.com>
2024-02-11 19:01:41 -0300

    Adding category product to header and changing the structure of styled

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -4,13 +4,13 @@ import styled from 'styled-components';
 const HeaderStyled = styled.header`
   display: flex;
   justify-content: space-around;
-  margin-top: 3.2rem;
   font-family: 'Poppins';
   flex-wrap: wrap;
 
-  article {
+`
+
+const Title = styled.article`
     margin: 1rem;
-  }
 
   h1 {
       color: ${({ theme }) => theme.colors.primaryColor};
@@ -19,7 +19,7 @@ const HeaderStyled = styled.header`
       margin-bottom: 1.2rem;
     }
 
-    article button {
+    button {
       background-color: ${({ theme }) => theme.colors.secondaryColor};
       border: none;
       border-radius: 8px;
@@ -31,7 +31,7 @@ const HeaderStyled = styled.header`
       font-size: 1.05rem;
     }
 
-    article a {
+    a {
       text-decoration: none;
       background-color: transparent;
       border: 1px solid;
@@ -44,13 +44,33 @@ const HeaderStyled = styled.header`
       font-family: 'Poppins';
     }
 
-    div {
+    @media (max-width: 650px) {
+      margin: 0;
+      width: 100%;
+      background: rgb(0,0,0);
+      background: linear-gradient(183deg, #000000a9 70%, #ffffff50 100%);
+      padding-bottom: 40vh;
+      z-index: 2;
+      text-align: center;
+
+      h1 {
+        margin-top: 1.5rem;
+        color: ${({ theme }) => theme.colors.mediumGray};
+      }
+
+      a {
+        color: ${({ theme }) => theme.colors.mediumGray};
+        border-color: ${({ theme }) => theme.colors.mediumGray};
+      }
+    }
+`
+
+const Image = styled.div`
       display: grid;
       margin: 1rem;
       z-index: 0;
-    }
 
-    div legend {
+    legend {
       background-color: ${({ theme }) => theme.colors.secondaryColor};
       justify-self: right;
       color: white;
@@ -61,48 +81,95 @@ const HeaderStyled = styled.header`
     }
 
     @media (max-width: 650px) {
-      margin-top: 0;
+      position: absolute;
 
-      article {
-        margin: 0;
-        width: 100%;
-        background: rgb(0,0,0);
-        background: linear-gradient(183deg, #000000a9 70%, #ffffff50 100%);
-        padding-bottom: 40vh;
-        z-index: 2;
-        text-align: center;
-      }
-
-      article h1 {
-        margin-top: 1.5rem;
-        color: ${({ theme }) => theme.colors.mediumGray};
-      }
-
-      article a {
-        color: ${({ theme }) => theme.colors.mediumGray};
-        border-color: ${({ theme }) => theme.colors.mediumGray};
-      }
-
-      div {
-        position: absolute;
+      legend {
+        display: none;
       }
     }
+
+`
+
+const ProductCategory = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+`
+
+const Product = styled.article`
+  cursor: pointer;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  border: 1px solid;
+  border-color: #A5A5A5;
+  border-radius: 1rem;
+
+  padding: .4rem 3rem;
+  width: 250px;
+  max-width: 300px;
+  margin: 0 .5rem;
+  margin-top: 2rem;
+
+    div {
+      white-space: nowrap;
+      margin-left: 2rem;
+      font-size: 1.1rem;
+      font-family: 'Poppins';
+      line-height: 1.6rem;
+      color: ${({ theme }) => theme.colors.primaryColor}
+    }
+
+  img {
+    width: auto;
+    max-width: 150px;
+  }
 `
 
 export default function Header() {
   return (
-    <HeaderStyled>
-      <article>
-        <h1>Canon <br />camera</h1>
-        <button>Add to cart</button>
-        <Link to="/">View more</Link>
-      </article>
+    <>
+      <HeaderStyled>
+        <Title>
+          <h1>Canon <br />camera</h1>
+          <button>Add to cart</button>
+          <Link to="/">View more</Link>
+        </Title>
 
-      <div>
-        <img src="/headerCam.png" alt="Canon cam" />
-        <legend>only<br />$88</legend>
-      </div>
+        <Image>
+          <img src="/headerCam.png" alt="Canon cam" />
+          <legend>only<br />$88</legend>
+        </Image>
 
-    </HeaderStyled>
+      </HeaderStyled>
+
+      <ProductCategory>
+
+        <Product>
+          <img src="/product/speaker.png" alt="Speaker" />
+          <div>
+            <h1>Speaker</h1>
+            <legend>(6 items)</legend>
+          </div>
+        </Product>
+
+        <Product>
+          <img src="/product/laptop.png" alt="Desktop and laptop" />
+          <div>
+            <h1>Desktop & laptop</h1>
+            <legend>(6 items)</legend>
+          </div>
+        </Product>
+
+        <Product>
+          <img src="/product/camera.png" alt="Camera" />
+          <div>
+            <h1>DSLR Camera</h1>
+            <legend>(6 items)</legend>
+          </div>
+        </Product>
+
+      </ProductCategory>
+    </>
   )
 }
